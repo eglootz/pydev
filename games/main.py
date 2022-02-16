1a7caa418acaad1de778658f401f9539cb35ee6b
eliasglootz <62054381+eliasglootz@users.noreply.github.com>
2022-02-16 13:31:26 +0100

    added a bird

--- games/main.py
+++ games/main.py
@@ -3,20 +3,31 @@ import pygame
 # Initialize the library
 pygame.init()
 
-# create a window with the size of (height=800,width=600)
+# create a window with the size of (width=800,height=600)
 screen = pygame.display.set_mode((800, 600))
 
 #title and icon
 pygame.display.set_caption("Space Invader")
 
+# Player
+playerImg = pygame.image.load("bird.png")
+playerX = 370
+playerY = 480
+
+
+def Player():
+    screen.blit(playerImg, (playerX, playerY))
+
 
 # game loop
 running = True
 while(running):
+
+    # RGB screen fill
+    screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # RGB screen fill
-    screen.fill((0, 0, 0))
+    Player()
     pygame.display.update()
