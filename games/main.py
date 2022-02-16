7396152910a46f83ca58950c1c494b2d7ad1586c
eliasglootz <62054381+eliasglootz@users.noreply.github.com>
2022-02-16 13:10:25 +0100

    added a bird

--- games/main.py
+++ games/main.py
@@ -6,9 +6,17 @@ pygame.init()
 # create a window with the size of (height=800,width=600)
 screen = pygame.display.set_mode((800, 600))
 
+#title and icon
+pygame.display.set_caption("Space Invader")
+
+
 # game loop
 running = True
 while(running):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # RGB screen fill
+    screen.fill((0, 0, 0))
+    pygame.display.update()
