18ecf384e0c502bd0df0b1db606fc33528d715b9
eliasglootz <62054381+eliasglootz@users.noreply.github.com>
2022-03-17 11:31:37 +0100

    changed images to log_images

--- light_control/cam.py
+++ light_control/cam.py
@@ -6,7 +6,7 @@ def picture(name):
     vc = cv2.VideoCapture(2)
     time.sleep(0.1)
     rval, frame = vc.read()
-    cv2.imwrite(f"images/{name}.jpg", frame)
+    cv2.imwrite(f"log_images/{name}.jpg", frame)
     cv2.destroyAllWindows()
     vc.release()
 
