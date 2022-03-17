b10f24fa68d68e6a8e82cf0c17e9b435986e82a1
eliasglootz <62054381+eliasglootz@users.noreply.github.com>
2022-03-17 14:23:44 +0100

    removed comments

--- light_control/color.py
+++ light_control/color.py
@@ -1,12 +1,6 @@
-#import tkinter as tk
 from tkinter.colorchooser import askcolor
 import light_control as lc
 import converter as cv
-
-
-# root = tk.Tk()
-# root.title('Tkinter Color Chooser')
-# root.geometry('300x150')
 
 
 def change_color():
@@ -14,14 +8,3 @@ def change_color():
     rgb = colors[0]
     coords = cv.rgb_to_xy(red=rgb[0], green=rgb[1], blue=rgb[2])
     lc.light(xy=coords)
-
-    return colors[1]
-
-
-# tk.Button(
-#     root,
-#     text="SELECT",
-#     command=change_color).pack(expand=True)
-
-
-# root.mainloop()
