960be0c549f10f01905ffa8cb6261c56e9573948
eliasglootz <62054381+eliasglootz@users.noreply.github.com>
2022-03-18 11:36:33 +0100

    .

--- light_control/screenshot_file.py
+++ light_control/screenshot_file.py
@@ -1,13 +1,59 @@
 import requests
+import urllib3
 
-light = "https://192.168.178.71/api/dCOuv5NAZaNSuZ5UGbB7dHEUfB182FjhMbPateTa/lights/1/state"
+# disabling warnings (certification issue)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-color = input("Enter a color: ")
 
-if color == "green":
-    data = '{"on":true, "sat":254, "bri":254,"hue":25500}'
+urls = [
+    "https://192.168.178.71/api/dCOuv5NAZaNSuZ5UGbB7dHEUfB182FjhMbPateTa/lights/1/state",
+    "https://192.168.178.71/api/dCOuv5NAZaNSuZ5UGbB7dHEUfB182FjhMbPateTa/lights/2/state",
+    "https://192.168.178.71/api/dCOuv5NAZaNSuZ5UGbB7dHEUfB182FjhMbPateTa/lights/3/state",
+    "https://192.168.178.71/api/dCOuv5NAZaNSuZ5UGbB7dHEUfB182FjhMbPateTa/lights/4/state"
+]
 
-elif color == "blue":
-    data = '{"on":true, "sat":254, "bri":254,"hue":46920}'
+print("Starting the program.")
 
-requests.put(light, data, verify=False)
+print("Turn lights on/off")
+
+state = input()
+
+if state.lower() == "on":
+    data = '{"on": true}'
+if state.lower() == "off":
+    data = '{"on": false}'
+
+for light in urls:
+    requests.put(light, data, verify=False)
+
+
+if state.lower() == "on":
+
+    print("Do you want to change the color? (y/n)")
+    answer = input()
+    if answer.lower() == "y":
+
+        print("Please enter a valid color:")
+        color = input()
+        if color.lower() == "yellow":
+            data = '{"on":true, "sat":254, "bri":254,"hue":12750}'
+
+        elif color.lower() == "green":
+            data = '{"on":true, "sat":254, "bri":254,"hue":25500}'
+
+        elif color.lower() == "blue":
+            data = '{"on":true, "sat":254, "bri":254,"hue":46920}'
+
+        elif color.lower() == "pink":
+            data = '{"on":true, "sat":254, "bri":254,"hue":56100}'
+
+        elif color.lower() == "red":
+            data = '{"on":true, "sat":254, "bri":254,"hue":65280}'
+
+        else:
+            print("Color currently not available.")
+
+        for light in urls:
+            requests.put(light, data, verify=False)
+
+print("Sucess.")
