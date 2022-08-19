7a0509ad29477220b549bb1b8bf4498dffae0df5
eliasglootz <62054381+eliasglootz@users.noreply.github.com>
2022-08-19 15:30:49 +0200

    cleaned up

--- light_control/first_program.py
+++ light_control/first_program.py
@@ -1,17 +1,12 @@
 import requests
 
-# disabling warnings (certification issue)
-#import urllib3
-# urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-# first program
 light = "http://192.168.178.71/api/JoSOxXkKb6sVDmTNEVsGel3sFh0wehyC3WHv0oAv/lights/1/state"
 color = input("Enter a color: ")
 
-if color.lower() == "green":
+if color == "green":
     data = '{"on":true, "sat":254, "bri":254,"hue":25500}'
 
-if color.lower() == "blue":
+if color == "blue":
     data = '{"on":true, "sat":254, "bri":254,"hue":46920}'
 
-requests.put(light, data)  # , verify=False)
+requests.put(light, data)
